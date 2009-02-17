17c2dede21fa9af4a09ba9e2909a000f8b928c8d
Liquidrums <eric@whitefence.com>
2009-02-17 11:58:35 -0500

    no message

---
+++ dojo/Button.js
@@ -0,0 +1,8 @@
+dojo.provide('my.form.Button');
+
+dojo.require('dijit.form.Button');
+
+dojo.declare('my.form.Button', dijit.form.Button, {
+    iconClass: 'red'
+    // Inherit all of the super class' code
+});
