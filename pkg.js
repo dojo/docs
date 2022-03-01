a743e1e3123803a89757c1e73202b3eb24f4b25d
Moejojojojo <100360853+Moejojojojo@users.noreply.github.com>
2022-02-28 22:17:12 -0600

    package.yam

---
+++ pkg.js
@@ -0,0 +1,438 @@
+Run:AUTOMATING...makefile_Rakegile_rubyGems.specs'@bitore.sig
+expand_all
+COMPARE MARKETS
+US
+Europe
+Asia
+Currencies
+Crypto
+Dow Jones
+33,892.60
+0.49%
+S&P 500
+4,373.94
+
+
+
+
+
+0.24%
+Nasdaq
+13,751.40
+0.41%
+Russell
+2,048.09
+0.35%
+VIX
+30.15
+9.28%
+Search for stocks, ETFs & more
+poll
+Your portfolios
+$94,063,923,772,175.80
+New portfolio
+ZAKWOOD
+$0.00
+0.00%
+https://www.google.com/API/***UNLIMITED** **AUTHOR***/v4/token
+$43,302,983,833,360.97
+0.16%
+const.items(((c)(r)))=:is::==yargs(AGS)).);. /
+$0.00
+0.00%
+zachryiixixiiwood@gmail.com
+$0.00
+0.00%
+Zachry Tyler Wood DOB 10 15 1994 SSID *****1752
+$24,830,612,401,044.03
+0.27%
+Copy of Zachry Tyler Wood DOB 10 15 1994 SSID *****1752
+$24,830,612,401,044.03
+0.27%
+JPMorgan/ZACHRY TYLER WOOD
+$0.00
+0.00%
+zachryTwood@gmail.com
+$0.00
+0.00%
+josephabanksfederalreserve@gmail.com
+$0.00
+0.00%
+string query Specifies the types of repositories you want retu
+$0.00
+0.00%
+From 9b8006bacb53ad543e7d63b4a640dcd82fb8b23a Mon Sep 17 00:00:0
+$549,857,525,250.00
+0.20%
+commit c3854d6c70ebe9b6fa2bed1a14d1fbfea97c6e9d
+$0.00
+0.00%
+bitcoin'
+$549,857,611,476.80
+0.20%
+Top movers in your lists
+GBTC
+Grayscale Bitcoin Trust (Btc)
+$29.39
+8.15%
+COIN
+Coinbase Global Inc
+$190.77
+7.88%
+TSLA
+Tesla Inc
+$870.43
+7.48%
+SQ
+Block Inc
+$127.50
+6.41%
+TWLO
+Twilio Inc
+$174.80
+4.56%
+C
+Citigroup Inc
+$59.23
+4.44%
+YOUR LISTS
+format_list_bulleted
+Watchlist
+57
+insert_chart
+ZAKWOOD
+0
+insert_chart
+https://www.google.com/API/***UNLIMITED** **AUTHOR***/v4/token
+3
+insert_chart
+const.items(((c)(r)))=:is::==yargs(AGS)).);. /
+1
+insert_chart
+zachryiixixiiwood@gmail.com
+12
+insert_chart
+Zachry Tyler Wood DOB 10 15 1994 SSID *****1752
+18
+insert_chart
+Copy of Zachry Tyler Wood DOB 10 15 1994 SSID *****1752
+19
+insert_chart
+JPMorgan/ZACHRY TYLER WOOD
+5
+format_list_bulleted
+Zachry Tyler Wood zachryTwood@gmail.com
+6
+insert_chart
+zachryTwood@gmail.com
+17
+insert_chart
+josephabanksfederalreserve@gmail.com
+8
+insert_chart
+string query Specifies the types of repositories you want retu
+15
+insert_chart
+From 9b8006bacb53ad543e7d63b4a640dcd82fb8b23a Mon Sep 17 00:00:0
+3
+insert_chart
+commit c3854d6c70ebe9b6fa2bed1a14d1fbfea97c6e9d
+0
+insert_chart
+bitcoin'
+11
+Market trends
+Today's financial news
+Top stories
+Local market
+World markets
+CNN
+11 hours ago
+Target boosts its starting pay for some jobs to $24 an hour
+TGT
+0.28%
+Target Corporation
+
+Decrypt
+3 hours ago
+Crypto Market Surge Continues as Bitcoin Tops $43K, Ethereum Nears $3K
+BTC / USD
+0.20%
+Bitcoin (BTC / USD)
+
+The Wall Street Journal
+3 hours ago
+Traders Make Millions by Short-Selling Russian Exchange-Traded Funds
+
+MarketWatch
+12 hours ago
+These are the most important S&P 500 and Nasdaq levels to watch right now, says this strategist
+.INX
+0.24%
+S&P 500
+
+CNBC
+6 hours ago
+Zoom provides disappointing revenue forecast for first quarter and full year
+ZM
+5.81%
+Zoom Video Communications Inc
+
+Reuters
+3 hours ago
+EXCLUSIVE MSCI says removing Russia from indexes 'natural next step'
+MSCI
+2.24%
+Msci Inc
+
+CNBC
+7 hours ago
+The average mortgage holder has a record $185,000 in equity. What to know if you’re tempted to borrow from that
+
+TechCrunch
+10 hours ago
+Orkes, founded by the creators of Netflix’s open source Conductor workflow orchestration tool, comes out of stealth with $9.3M
+
+The Wall Street Journal
+17 hours ago
+How to Prepare for Buy-Now-Pay-Later Purchases Showing Up on Credit Reports
+EXPN
+0.35%
+Experian plc
+
+Earnings calendar
+Based on popular stocks and your watchlists
+MAR
+1
+Baidu
+Mar 1, 2022, 6:30 AM
+MAR
+1
+Sea
+Mar 1, 2022, 6:30 AM
+MAR
+1
+Salesforce
+Mar 1, 2022, 4:00 PM
+MAR
+2
+Snowflake
+Mar 2, 2022, 4:00 PM
+MAR
+8
+MongoDB Inc.
+Mar 8, 2022, 4:00 PM
+MAR
+10
+DocuSign
+Mar 10, 2022, 3:30 PM
+Most followed on Google
+AAPL
+Apple Inc
+3.71M following
+0.16%
+GOOGL
+Alphabet Inc Class A
+2.16M following
+0.44%
+MSFT
+Microsoft Corporation
+1.84M following
+0.50%
+AMZN
+Amazon.com, Inc.
+1.74M following
+0.15%
+FB
+Meta Platforms Inc
+1.58M following
+0.26%
+TSLA
+Tesla Inc
+1.49M following
+7.48%
+Market trends
+Most active
+Gainers
+Losers
+Trending
+GRAB
+Grab Holdings Ltd
+$5.76
+0.69%
+AMD
+Advanced Micro Devices, Inc.
+$123.34
+1.88%
+Semiconductor sales to Russia banned, but that shouldn't hurt Intel, AMD and other chip makers
+MarketWatch • 7 hours ago
+FHN
+First Horizon Corp
+$23.48
+28.66%
+First Horizon Stock Surges on Deal to Be Bought by TD Bank
+Barron's • 8 hours ago
+AAPL
+Apple Inc
+$165.12
+0.16%
+Apple Inc. stock rises Monday, still underperforms market
+MarketWatch • 6 hours ago
+NIO
+Nio Inc - ADR
+$22.84
+9.07%
+NIO, Novavax, Lockheed, Yandex, First Horizon: What to Watch in the Stock Market Today
+The Wall Street Journal • 13 hours ago
+F
+Ford Motor Company
+$17.56
+1.51%
+Discover more
+You may be interested in
+CVX
+Chevron Corporation
+$144.00
+2.58%
+UNH
+UnitedHealth Group Inc
+$475.87
+0.025%
+TCEHY
+Tencent Holdings ADR
+$53.79
+1.28%
+ATVI
+Activision Blizzard, Inc.
+$81.50
+0.074%
+MCD
+McDonald's Corp
+$244.77
+1.33%
+MU
+Micron Technology, Inc.
+$88.86
+1.38%
+AVGO
+Broadcom Inc
+$587.44
+0.097%
+CMCSA
+Comcast Corporation
+$46.76
+0.66%
+TGT
+Target Corporation
+$199.77
+0.28%
+KO
+Coca-Cola Co
+$62.24
+0.97%
+LRCX
+Lam Research Corporation
+$561.35
+2.07%
+MRK
+Merck & Co., Inc.
+$76.58
+0.34%
+AAL
+American Airlines Group Inc
+$17.25
+1.20%
+NXST
+Nexstar Media Group Inc
+$185.05
+3.65%
+XOM
+Exxon Mobil Corp
+$78.42
+0.75%
+T
+AT&T Inc.
+$23.69
+0.92%
+INDEX
+VIX
+30.15
+9.28%
+CCL
+Carnival Corp
+$20.33
+3.65%
+Recently searched
+INDEX
+S&P 500
+4,373.94
+0.24%
+SEEGX
+JPMorgan Large Cap Growth Fund Class I
+$53.87
+2.07%
+OLGAX
+JPMorgan Large Cap Growth Fund Class A
+$52.49
+2.06%
+ZNGA
+Zynga Inc
+$9.08
+1.09%
+INDEX
+Dow Jones Industrial Average
+33,892.60
+0.49%
+VWINX
+Vanguard Wellesley® Income Fund Investor Shares
+$28.00
+1.27%
+JHEQX
+JPMorgan Hedged Equity Fund Class I
+$25.90
+0.66%
+VCAIX
+Vanguard California Intermediate-Term Tax-Exempt Fund Investor Shares
+$11.81
+0.085%
+XM
+Qualtrics International Inc
+$30.33
+2.12%
+ARKG
+ARK Genomic Revolution ETF
+$48.38
+1.92%
+SPAQ.UN
+Spartan Acquisition III Units
+$10.18
+0.098%
+SPAQ
+Spartan Acquisition III Corp
+$9.96
+0.10%
+MSTGX
+Morningstar Global Income Fund
+$10.45
+1.46%
+MSTVX
+Morningstar Alternatives Fund
+$10.21
+0.20%
+LEAP
+Ribbit LEAP Ltd
+$9.86
+0.00%
+PSTH
+Pershing Square Tontine Holdings Ltd
+$19.81
+0.10%
+BITW
+Bitwise 10 Crypto Index Units Beneficial Interest
+$31.72
+3.36%
+BRPHF
+Galaxy Digital Holdings Ltd
+$13.00
+8.79%//Running...Installm I stall_$ -cd:: AUTOMAT ALL
+ALL:/
